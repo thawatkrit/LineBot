6cd7d56fbf321b44d55a6e69fbb3bf2a58f447f4
thawatkrit <thawatkritrubang@gmail.com>
2018-12-19 05:47:16 +0700

    fix error

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
     require 'messageController.php';
 
     foreach($userList as $user) {
-        echo '<h2>'.$user.'</h2>';
+        echo '<h2>'.$user->getUserId().'</h2>';
         foreach($user->getMessages() as $message) {
             echo '<p>'.$message.'</p>';
         }
