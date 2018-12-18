8bd920c3ea3fd3ecff40976dc33782700592e089
thawatkrit <thawatkritrubang@gmail.com>
2018-12-19 05:45:17 +0700

    fix error

--- admin.php
+++ admin.php
@@ -3,7 +3,7 @@
 
     foreach($userList as $user) {
         echo '<h2>'.$user.'</h2>';
-        foreach($user->getMessages as $message) {
+        foreach($user->getMessages() as $message) {
             echo '<p>'.$message.'</p>';
         }
     }
